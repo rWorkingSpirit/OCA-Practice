f2361633e5b60d199c391f644dfd9ec6fc4c4d0a
roy <r.rooseboom@workingspirit.nl>
2023-02-20 11:42:04 +0100

    First commit for BasicsEx class

--- src/StartPoint.java
+++ src/StartPoint.java
@@ -1,13 +1,14 @@
 // Default package has no 'package xxx'.
 
+import basics.*;
+
 
 public class StartPoint 
 {
 	
 	public static void main(String[] args)
 	{
-		
+		BasicsEx basicEX = new BasicsEx();
 	}
-	
 	
 }
